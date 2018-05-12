ee9de906de67be0933123afb44942f9beeb19b47
yz9 <yzdslloli@163.com>
2018-05-12 21:41:41 +0800

    取消收费

--- bootstrapTest/others/js/music.js
+++ bootstrapTest/others/js/music.js
@@ -365,9 +365,9 @@ let swapsong=songCantPlay;
 let setTimeText=function(){
     nowTime.text(getTime(Math.floor(player.position/1000)));
     progressNow.css("width",(parseInt(player.position)/playTime*100).toFixed(0)+"%");
-    if(player.readyState==2){
+    /* if(player.readyState==2){
         swapsong();
-     } 
+     }  */
     if(player.position+1500>playTime){
         nexx();
         player.setPosition(0);
